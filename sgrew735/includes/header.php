8219eacaa3115f1d9391e4c1b0efd443a2b9bac4
Sahil Grewal <sgrew735@mtroyal.ca>
2024-10-18 15:23:58 -0600

    added the css link to the header

--- sgrew735/includes/header.php
+++ sgrew735/includes/header.php
@@ -1,10 +1,21 @@
-<header>
-    <h1>F1 Dashboard Project</h1>
-    <nav>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="browse.php">Browse</a></li>
-            <li><a href="apiTester.php">APIs</a></li>
-        </ul>
-    </nav>
-</header>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css\styles.css">
+    <title>F1 Dashboard Project</title>
+</head>
+
+<body>
+    <header>
+        <h1>F1 Dashboard Project</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="browse.php">Browse</a></li>
+                <li><a href="apiTester.php">APIs</a></li>
+            </ul>
+        </nav>
+    </header>
